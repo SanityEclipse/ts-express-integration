57ab90a03416d9b37f94cd6fdca720d9f1beab89
Thomas N Rizzo III <thomas.rizzoiii@centurylink.com>
2022-01-18 15:37:22 -0700

    POC refactor of login page with decorators.

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,16 @@ import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 
+import { router as controllerRouter } from './controllers/decorators/Controller';
+import './controllers/LoginController';
+
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['cookieString'] }));
 app.use(router);
+app.use(controllerRouter);
 
 app.listen(3000, () => {
   console.log('listening on port 3000')
