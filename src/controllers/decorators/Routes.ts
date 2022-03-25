8d78d14073fb02e309f3007a38fc0e952773724c
Thomas N Rizzo III <thomas.rizzoiii@centurylink.com>
2022-03-25 14:07:01 -0600

    Added 'use' method for adding middleware via metadata.

--- src/controllers/decorators/Routes.ts
+++ src/controllers/decorators/Routes.ts
@@ -1,11 +1,12 @@
+import { MetadataKeys } from './MetadataKeys';
 import 'reflect-metadata';
 import { Methods } from './Methods';
 
 function routeBinder(method: string) {
   return function (path: string) {
     return function (target: any, key: string, desc: PropertyDescriptor) {
-      Reflect.defineMetadata('path', path, target, key);
-      Reflect.defineMetadata('method', method, target, key);
+      Reflect.defineMetadata(MetadataKeys.path, path, target, key);
+      Reflect.defineMetadata(MetadataKeys.method, method, target, key);
     };
   }
 }
